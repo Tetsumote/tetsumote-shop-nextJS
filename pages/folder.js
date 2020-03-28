3900d58da3ecefd41eb103efe15d88aa15d644a9
Tetsumote <unionproject88@gmail.com>
2020-03-28 10:08:04 +0100

    add starter

--- pages/folder.js
+++ pages/folder.js
@@ -1,4 +1,4 @@
 import withGraphQLAndBasket from 'lib/with-graphql-and-basket';
-import Folder from 'page-components/folder';
+import Folder from 'page-components/folder'; 
 
 export default withGraphQLAndBasket(Folder);
